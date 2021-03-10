0acf37476a0958c0a1ce032bb71810cdfa9894c2
marcelo1811 <miyachimarcelo@gmail.com>
2021-03-10 11:59:06 -0300

    feat(RespositoryList): add fetch repositories

--- 01-github-explorer/src/components/RespositoryList.jsx
+++ 01-github-explorer/src/components/RespositoryList.jsx
@@ -1,13 +1,25 @@
 import { RepositoryItem } from "./RepositoryItem"
 import '../styles/repositories.scss'
+import { useEffect, useState } from "react"
+
+// https://api.github.com/orgs/rocketseat/repos
 
 const repository = {
   name: 'unform',
   description: 'Forms in React',
-  link: 'https://wwww.github.com/marcelo1811'
+  link: 'https://wwww.github.com/unform/unform'
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [repositories])
+
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
